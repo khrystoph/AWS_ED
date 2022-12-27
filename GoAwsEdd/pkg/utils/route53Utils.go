5abb6b7f5e78498297938d99ee65f0cbd54bc71e
Bret Edwards <sliverman69@gmail.com>
2022-12-26 21:36:32 -0600

    route53Utils.go: UpdateResourceRecordSet updates the resource records passed into the function as a batched job

--- GoAwsEdd/pkg/utils/route53Utils.go
+++ GoAwsEdd/pkg/utils/route53Utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/route53"
@@ -81,4 +82,58 @@ func RetrieveResourceRecordSet(zoneID, baseDomain, rrType string) (resourceRecor
 	}
 
 	return resourceRecord, nil
+}
+
+// UpdateResourceRecordSet takes the resourceRecordSet ID and current IP address to update the mismatched record
+// in route53 with the correct IP for the resource record set type and domain/subdomain.
+func UpdateResourceRecordSet(resourceRecordSets []types.ResourceRecordSet, hostedZoneID *string) (requestID string, err error) {
+	var (
+		rrSetChanges            []types.Change
+		rrSetChangeBatch        = &types.ChangeBatch{Changes: rrSetChanges}
+		resourceRecordSetInputs = &route53.ChangeResourceRecordSetsInput{
+			ChangeBatch:  rrSetChangeBatch,
+			HostedZoneId: hostedZoneID}
+	)
+	svcClient, err := Route53Client()
+	if err != nil {
+		fmt.Printf("error setting up route53 client to update resource record sets. Error msg: %s\n", err)
+		return "", err
+	}
+	if !(len(resourceRecordSets) >= 1) {
+		err = errors.New("no resource record sets provided as input to function. bailing out")
+		return "", err
+	}
+	for index := range resourceRecordSets {
+		change := types.Change{
+			Action:            "UPSERT",
+			ResourceRecordSet: &resourceRecordSets[index],
+		}
+		rrSetChanges = append(rrSetChanges, change)
+	}
+
+	resourceRecordSetInputs.ChangeBatch.Changes = rrSetChanges
+
+	//print intput as JSON as a debug message to ensure the inputs are going in correctly
+	rrSetInputsJSON, err := json.MarshalIndent(resourceRecordSetInputs, "", "  ")
+	if err != nil {
+		fmt.Printf("Warning: unable to marshal inputs as JSON. Error msg: %s\n", err)
+	} else {
+		fmt.Printf("Resource Record Set Inputs: \n%s\n", rrSetInputsJSON)
+	}
+
+	resourceRecordSetUpdateOutput, err := svcClient.ChangeResourceRecordSets(context.Background(), resourceRecordSetInputs)
+	if err != nil {
+		fmt.Printf("error updating resource record sets. Error msg: %s", err)
+		return "", err
+	}
+
+	resourceRecordSetUpdateOutputJSON, err := json.MarshalIndent(resourceRecordSetUpdateOutput, "", "  ")
+	if err != nil {
+		fmt.Printf("unable to response output JSON. Error msg: %s\n", err)
+	} else {
+		fmt.Printf("resource record sets updated. Following output returned: %s\n", resourceRecordSetUpdateOutputJSON)
+	}
+
+	requestID = *resourceRecordSetUpdateOutput.ChangeInfo.Id
+	return requestID, nil
 }
